6d52819017d77173c1e557cdcc151720daf170d1
Francisco Ribeiro Junior <f.ribeiro1205@gmail.com>
2021-09-05 23:43:19 -0300

    Adicionar arquivos de projeto.

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatSignalRCore.Hubs
+{
+    public class ChatHub : Hub
+    {
+        //Metodo de envio de mensagens
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        //Metodo de envio de mensagens privadas
+        public Task SendPrivateMessage(string user, string message)
+        {
+            return Clients.User(user).SendAsync("ReceiveMessage", message);
+        }
+
+        //Adicionando um "grupo"
+        public async Task AddToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has joined the group {groupName}.");
+        }
+
+        //Removendo um "grupo"
+        public async Task RemoveFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has left the group {groupName}.");
+        }
+    }
+}
